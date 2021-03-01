58ccc7d574be282395c8865d3dca4952fc26a3ce
wzj <1121608692@qq.com>
2021-03-01 11:30:13 +0800

    完成了首页的创建

---
+++ assets/js/index.js
@@ -0,0 +1,55 @@
+$(() => {
+        getUserinfo()
+            // 3.实现退出功能
+            // 3.1点击退出按钮实现退出功能
+        $('#btnLogout').click(() => {
+            layer.confirm('确定退出登录?', { icon: 3, title: '提示' }, function(index) {
+                //do something
+                // 3.2. 清空本地存储中的 token
+                localStorage.removeItem('token')
+
+                // 3.3. 重新跳转到登录页面
+                location.href = '/login.html'
+
+                // 3.4关闭 confirm 询问框
+                layer.close(index);
+            });
+        })
+    })
+    // 1.定义获取用户信息的函数 getUserinfo 函数
+
+
+function getUserinfo() {
+    // 1.1发起get请求
+    $.ajax({
+        type: "get",
+        url: "/my/userinfo",
+        success: res => {
+            if (res.status !== 0) return layui.layer.msg(res.message)
+
+            // 1.2渲染用户头像
+            renderAvatar(res.data)
+        }
+    });
+};
+// 2.定义渲染用户信息头像的函数 renderAvatar 函数
+function renderAvatar(data) {
+    let name = data.nickname || data.username
+
+    // 2.1设置欢迎文本
+    $('.welcome').html('欢迎你&nbsp;&nbsp;' + name)
+
+    // 2.2判断用什么头像
+    if (data.user_pic !== null) {
+
+        // 2.3使用图像头像
+        $('.layui-nav-img').prop('src', data.user_pic).show()
+        $('.text-avatar').hide()
+    } else {
+
+        // 2.4使用文字头像
+        let first = name[0].toUpperCase()
+        $('.layui-nav-img').hide()
+        $('.text-avatar').html(first).show()
+    }
+};
---
+++ assets/js/user/user_info.js
@@ -0,0 +1,54 @@
+$(function() {
+    // 1.昵称的表单验证
+    let form = layui.form
+
+    form.verify({
+        nickname: function(value) {
+            if (value.length > 6) return '昵称长度必须在 1 ~ 6 个字符之间！'
+        }
+    });
+
+    initUserInfo()
+
+    // 2.初始化用户的基本信息
+    function initUserInfo() {
+        $.ajax({
+            method: 'GET',
+            url: '/my/userinfo',
+            success: function(res) {
+                if (res.status !== 0) return layui.layer.msg()('获取用户信息失败！')
+                console.log(res)
+
+                // 2.1.使用form.val方法快速为表单赋值
+                form.val('formUserInfo', res.data)
+            }
+        });
+    };
+    // 3.实现表单的重置效果
+    $('#btnReset').click(e => {
+        // 阻止表单的默认重置行为
+        e.preventDefault()
+        initUserInfo()
+    });
+    // 4.发起请求更新用户的信息
+    // 4.1监听表单的提交事件
+    $('.layui-form').on('submit', function(e) {
+        // 阻止表单的默认提交行为
+        e.preventDefault()
+
+        // 发起 ajax 数据请求
+        $.ajax({
+            method: 'POST',
+            url: '/my/userinfo',
+            data: $('.layui-form').serialize(),
+            success: function(res) {
+                if (res.status !== 0) return layui.layer.msg('更新用户信息失败！')
+                layui.layer.msg('更新用户信息成功！')
+
+                // <iframe>中的子页面，如果想要调用父页面中的方法，使用 window.parent 即可
+                // 调用父页面中的方法，重新渲染用户的头像和用户的信息
+                window.parent.getUserinfo()
+            }
+        })
+    })
+})
